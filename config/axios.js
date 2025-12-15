608608d3d9f49fc6300dff1fb9e0a1e3c9c5c536
MECKY <Melikizedekif@gmail.com>
2025-12-15 17:21:34 +0300

    Update axios.js

--- config/axios.js
+++ config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://emsbackend-mgsf.onrender.com/",
+  baseURL: "https://emsbackend-mgsf.onrender.com/api",
   withCredentials: true, // keep true only if you use cookies
 });
 
