2334e4f3bfcc34a19bb82a0dd1567ed8df89dfdb
MECKY <Melikizedekif@gmail.com>
2025-12-15 17:03:39 +0300

    Update axios.js

--- config/axios.js
+++ config/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://emsbackend-mgsf.onrender.com/",
-  withCredentials: false, // keep true only if you use cookies
+  withCredentials: true, // keep true only if you use cookies
 });
 
 export default api;
