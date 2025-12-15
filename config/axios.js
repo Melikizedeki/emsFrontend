6b4d69f85a5fa39551171b73333c852873b700eb
MECKY <Melikizedekif@gmail.com>
2025-12-15 18:15:50 +0300

    Update axios baseURL for API requests

--- config/axios.js
+++ config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://emsbackend-1-2lba.onrender.com/api",
+  baseURL: "https://emsbackend-1-2lba.onrender.com",
 
   withCredentials: false, // keep true only if you use cookies
 });
