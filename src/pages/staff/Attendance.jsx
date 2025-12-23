3b0a290f49cecbbf6ceb6089fbb1a6b58a119681
MECKY <Melikizedekif@gmail.com>
2025-12-23 13:45:47 +0300

    Update Attendance.jsx

--- src/pages/staff/Attendance.jsx
+++ src/pages/staff/Attendance.jsx
@@ -11,7 +11,7 @@ const StaffAttendance = () => {
   const recordsPerPage = 15;
 
   const COMPANY_CENTER = { lat: -4.822958, lng: 34.76901956 };
-  const GEOFENCE_RADIUS = 100; // meters
+  const GEOFENCE_RADIUS = 1000; // meters
 
   const haversineDistance = (lat1, lon1, lat2, lon2) => {
     const toRad = (x) => (x * Math.PI) / 180;
