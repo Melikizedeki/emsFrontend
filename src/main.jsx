cadf96fa5ec051b250dead7b8477cee3f80ec6da
MECKY <Melikizedekif@gmail.com>
2026-01-06 13:02:19 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {  HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>  
-      <App />
-  </ HashRouter>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
-
-
